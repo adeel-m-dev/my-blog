16f01c7b2d811abdf3511aaf6e9c58a957147f27
Adeel Manzoor <adeel.m.dev@gmail.com>
2023-12-06 12:59:19 +0500

    refactor[Card] deleted

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,6 @@
 import Navbar from "../components/AuthNavbar";
 import Footer from "../components/Footer";
-import StepFive from "../components/Steps/StepFive";
-import StepFour from "../components/Steps/StepFour";
 import StepOne from "../components/Steps/StepOne";
-import StepSix from "../components/Steps/StepSix";
 import StepThree from "../components/Steps/StepThree";
 import StepTwo from "../components/Steps/StepTwo";
 
